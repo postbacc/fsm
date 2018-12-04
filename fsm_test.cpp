1cb2bb8ec1ad9a0dbcf988f55fb353dc174228a8
Thomas Nelson <thomas.nelson@colorado.edu>
2018-12-04 12:14:48 -0700

    Move #define private public to fix build errors

--- fsm_test.cpp
+++ fsm_test.cpp
@@ -8,9 +8,9 @@
 // mechanisms. This is acceptable for unit testing, but not much
 // else. It is also not portable because it is not supported by the
 // C++ spec, and therefore might not work with all compilers.
-#define private public
 
 #include "catch.hpp"
+#define private public
 #include "fsm.hpp"
 
 using namespace std;
